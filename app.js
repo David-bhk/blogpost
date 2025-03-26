478e4d1cf862ea004ba0c435170cb8cee008035b
David Bihako <davidbihako4@gmail.com>
2025-03-26 10:48:01 +0200

    creating views and config express routing

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.set("views engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index.ejs");
+});
+
+app.get("/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/create", (req, res) => {
+  const { title, content } = req.body;
+  console.log(`blog post: ${title}\n${content}`);
+  res.redirect("/");
 });
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
